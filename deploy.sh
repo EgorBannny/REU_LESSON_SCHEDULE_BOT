8890d16598a10290ea519af5e1a77cfee53e8b03
ommm <ommm@ssh1.paris1.alwaysdata.com>
2026-09-02 21:51:03 +0200

    test cd workflow

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+cd ~/
+
+echo "=== Обновление кода ==="
+cp .env /tmp/.env.backup 2>/dev/null || true
+
+git fetch origin main
+git reset --hard origin/main
+
+cp /tmp/.env.backup .env 2>/dev/null || echo "WARN: .env не найден в бэкапе!"
+
+echo "=== Перезапуск Бот-сервиса ==="
+curl -X POST --basic --user "${ALWAYSDATA_TOKEN} account=${ACCOUNT_NAME}:" https://api.alwaysdata.com/v1/service/${SERVICE_ID}/restart/
+echo "Запрос на перезапуск сервиса ${SERVICE_ID} отправлен."
