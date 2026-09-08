5691ba609dcb9328c2f84414d426c0e7efeecf88
denisbiceps <slox0904@gmail.com>
2026-09-09 02:43:06 +0300

    hotfix/app.py

--- schedule_bot/telegram/app.py
+++ schedule_bot/telegram/app.py
@@ -11,6 +11,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import BotCommand
+from aiogram.client.session.aiohttp import AiohttpSession
 
 from .. import db
 from ..config import BOT_TOKEN, DATABASE_URL, TELEGRAM_PROXY
